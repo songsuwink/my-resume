14ff2b8f9d4a2b523b483b6b154f3a5bc25643c6
songsuwink <songsuwinkluey@gmail.com>
2023-07-24 16:26:17 +0700

    Add MUI

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+const fontKanit = "'Kanit', sans-serif";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: ["'Kanit', sans-serif"].join(","),
+  },
+});
